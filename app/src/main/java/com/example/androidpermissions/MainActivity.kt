47a33f655fe65ce4bba3340ee6042a2097357c05
Johnny 3 Young <youngerblack@gmail.com>
2019-11-29 09:53:10 -0500

    adding project with permission

---
+++ app/src/main/java/com/example/androidpermissions/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.androidpermissions
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+
+    private val CAMERA_REQUEST_CODE = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        button_camera.setOnClickListener {
+
+            checkCameraPermission()
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            //El permiso no está aceptado
+        } else {
+            //El permiso está aceptado
+        }
+        //Tenemos que añadir otro if para comprobar si es la primera vez que pulsa el botón y saber si lo ha rechazado anteriormente,
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, android.Manifest.permission.CAMERA
+            )
+        ) {
+            //El usuario ya ha rechazado el permiso anteriormente, debemos informarle que vaya a ajustes.
+        } else {
+            //El usuario nunca ha aceptado ni rechazado, así que le pedimos que acepte el permiso.
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //El usuario ha aceptado el permiso, no tiene porqué darle de nuevo al botón, podemos lanzar la funcionalidad desde aquí.
+                } else {
+                    //El usuario ha rechazado el permiso, podemos desactivar la funcionalidad o mostrar una vista/diálogo.
+                }
+                return
+            }
+            else -> {
+                // Este else lo dejamos por si sale un permiso que no teníamos controlado.
+            }
+        }
+    }
+}
